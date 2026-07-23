ed90fbfb5e6c2ec339119b004507f78170f6ed18
kirubakaran232 <skirubakaran232@gmail.com>
2026-07-23 16:30:50 +0530

    add link for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { auth } from '../config/firebase';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://hr-recruiter-student-recommendation.onrender.com/api'
 });
 
 api.interceptors.request.use(async (config) => {
